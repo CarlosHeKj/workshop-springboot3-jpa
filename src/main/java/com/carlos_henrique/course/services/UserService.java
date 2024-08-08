869760215faabf79d9bb18f807639b0b66573b2c
CarlosHeKj <carloslouise2015@gmail.com>
2024-08-08 14:06:11 -0300

    Exception handling - update

--- src/main/java/com/carlos_henrique/course/services/UserService.java
+++ src/main/java/com/carlos_henrique/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.carlos_henrique.course.repositories.UserRepository;
 import com.carlos_henrique.course.services.exceptions.DatabaseException;
 import com.carlos_henrique.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -39,10 +41,13 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity,obj);
 		return repository.save(entity);
-		
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
