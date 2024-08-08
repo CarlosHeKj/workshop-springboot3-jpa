aa5ee8987b0122b3212149a1a5d6fe6a94087434
CarlosHeKj <carloslouise2015@gmail.com>
2024-08-08 13:49:04 -0300

    Exception hadling - findById

--- src/main/java/com/carlos_henrique/course/services/UserService.java
+++ src/main/java/com/carlos_henrique/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.carlos_henrique.course.entities.User;
 import com.carlos_henrique.course.repositories.UserRepository;
+import com.carlos_henrique.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	public User insert(User obj) {
 		return repository.save(obj);
