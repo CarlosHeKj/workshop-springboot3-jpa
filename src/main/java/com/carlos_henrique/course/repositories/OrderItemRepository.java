44d04d0509fbd771690428db52d5f18cecc21ed3
CarlosHeKj <carloslouise2015@gmail.com>
2024-08-12 13:55:22 -0300

    corrigindo erros

--- src/main/java/com/carlos_henrique/course/repositories/OrderItemRepository.java
+++ src/main/java/com/carlos_henrique/course/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.carlos_henrique.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.carlos_henrique.course.entities.OrderItem;
+import com.carlos_henrique.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 	
 	
